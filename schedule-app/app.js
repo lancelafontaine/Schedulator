91cc324a3e42f1befcf6575b6440a3430c6cb1b4
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-02-06 12:39:00 -0500

    [#17] Remakes directory structure of views and head, header and footer files.

--- schedule-app/app.js
+++ schedule-app/app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public

22f8a2051dbbae6621664c922e6c9639f3704f99
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2016-02-08 19:35:36 -0500

    user preference page debugging

--- schedule-app/app.js
+++ schedule-app/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var setting = require('./routes/setting');
 
 var app = express();
 

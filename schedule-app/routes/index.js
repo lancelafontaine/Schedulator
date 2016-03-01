33c0a586a2554890c1586789058fa1fd130d5acc
Andrew Laramee <zephix109@gmail.com>
2016-03-01 13:22:32 -0500

    [#14] User authentication, continued implementation
    Added basic signup.ejs page
    
    Added route for signup.ejs
    
    Added logout feature, controlled by passport

--- schedule-app/routes/index.js
+++ schedule-app/routes/index.js
@@ -2,17 +2,23 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. (with login section) */
 router.get('/', function(req, res, next) {
   res.render('index', {});
 });
+
+//process login form
+// app.post('/login', passport work here);
+
 
 /* GET user setting page. */
 router.get('/setting', function(req, res, next) {
   res.render('setting', {});
 });
 
-/* GET account page. (after login) */
+/* GET account page. (after login) 
+ * Protected section, must be logged in to access
+ */
 router.get('/account', isLoggedIn, function(req, res, next){
 	res.render('account', {
 		user : req.user // get user out of session and pass to template

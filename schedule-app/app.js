be5c24d6f2dcf61f514a80e335889d859fbeec14
Andrew Laramee <zephix109@gmail.com>
2016-03-01 12:01:58 -0500

    [#14] Introduced partial authentication process
    Required to proceed:
    -database information
    -user model
    
    Added error handlers to app.js
    
    Added bcrypt-node.js package to handle encryption for authentication
    
    Added unfinished routes for account and signup page
    
    Added isLoggedIn function to aid authentication
    
    Added account.ejs, simple account page

--- schedule-app/app.js
+++ schedule-app/app.js
@@ -14,4 +14,42 @@ app.set('view engine', 'ejs');
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// required for passport
+/*	app.use(session({ secret: 'iambutterfly' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
 module.exports = app;
--- schedule-app/routes/index.js
+++ schedule-app/routes/index.js
@@ -12,4 +12,27 @@ router.get('/setting', function(req, res, next) {
   res.render('setting', {});
 });
 
+/* GET account page. (after login) */
+router.get('/account', isLoggedIn, function(req, res, next){
+	res.render('account', {
+		user : req.user // get user out of session and pass to template
+	});
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next){
+	res.render('signup.ejs', {});
+});
+
+//check if user is logged in
+function isLoggedIn(req, res, next){
+	
+	//if user is authenticated, continue
+	if(req.isAuthenticated())
+		return next();
+	
+	//if user is not authenticated
+	res.redirect('/');
+}
+
 module.exports = router;

9310f494539007bbd295743396ddf67d71fdc542
Nassim <nassim.elsayed@hotmail.com>
2016-03-19 18:42:55 -0400

    Created model files for every class used in the backend. I also put in the comments an explanation of what the functions do. If you start working on a class communicate with your teammates please! I'm not sure if every object has to be created with the user parameters so feel free to change whatever you want

--- schedule-app/models/admin.js
+++ schedule-app/models/admin.js
@@ -10,12 +10,18 @@ admin = {
   listOfOfferedCourses: [],
   
   // methods
+
+  //Displays the list of courses from the course catalogue object which represents the engineering sequence
   viewCourses: function () {
     //
   },
+
+  //Allows an admin to modify the engineering sequence by removing a course
   removeCourse: function () {
     //
   },
+
+  //Allows an admin to modify the course sequence by adding a course or adding a section to an exsiting course
   addCourse: function () {
     //
   }
---
+++ schedule-app/models/autorization.js
@@ -0,0 +1,21 @@
+//var user = require('user');
+var autorization = Object.create(user);
+autorization = {
+
+  //No properties
+  
+  // methods
+
+  //Verifies if the user session is still valid
+  isLogged: function () {
+    //
+  },
+
+  //Secures the calls before they are proceeded to access the data
+  authorization: function () {
+    //
+  },
+ 
+}
+
+module.exports = authorization;
